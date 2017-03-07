1c075da65368d3968637d0dd5d40dffef8fae86b
excellencetechnologies <vikas@excellencetechnologies.in>
2017-03-07 10:47:57 +0530

    :wq

--- json_assignment1.php
+++ json_assignment1.php
@@ -6,25 +6,25 @@
 	<script type="text/javascript" src="../jquery-3.1.1.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function(){
-			$("#json").click(function(){
+			$("#getdata").click(function(){
 				$.getJSON("egjson.json",function(data){
 					$("#jsonname").append(data.firstname+"<br>");
-					$("#jsonage").append(data.lastname+"<br>");
-					$("#jsongender").append(data.dob);
+					$("#jsonlastname").append(data.lastname+"<br>");
+					$("#jsondob").append(data.dob);
 				});
-				$("#json1").hide();
-				$(".jjson").show();
+				$("#message").hide();
+				$(".json_data").show();
 			});
 		});
 	</script>
 </head>
 <body>
-<span id="json1">To Get Data Click On Button</span><br>
-<div class="jjson" hidden>
+<span id="message">To Get Data Click On Button</span><br>
+<div class="json_data" hidden>
 	<span id="jsonname">First Name is  </span>
-	<span id="jsonage">Last Name is  </span>
-	<span id="jsongender">Date Of Birth is  </span>
+	<span id="jsonlastname">Last Name is  </span>
+	<span id="jsondob">Date Of Birth is  </span>
 </div>
-<input type="button" name="json" value="Get Data" id="json">
+<input type="button" name="getdata" value="Get Data" id="getdata">
 </body>
 </html>
